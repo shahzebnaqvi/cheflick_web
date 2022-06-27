a778ab9841505c9fc5f1ed954457c0d15a5a23c1
unknown <shahzeb.naqvi12@gmail.com>
2022-06-27 21:38:08 +0500

    a

--- components/modals/map-modal.php
+++ components/modals/map-modal.php
@@ -1,12 +1,72 @@
+
+<?php 
+if(isset($_POST['search']))
+{
+     $search = $_POST['search'];
+
+        $search = str_replace(" ", "+", $search);
+       $iframe= '<iframe width="100%" height="500" src="https://maps.google.com/maps?q='.$search.'&output=embed"></iframe>';
+
+
+}
+else {
+  $iframe=' <iframe class="map-custom" width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(Sofrah)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+    </iframe>';
+}
+?>
+
+<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+    <script>
+      $(function () {
+
+        $('form').on('submit', function (e) {
+
+          e.preventDefault();
+
+          $.ajax({
+            type: 'post',
+            url: 'post.php',
+            data: $('form').serialize(),
+            success: function () {
+              alert('form was submitted');
+            }
+          });
+
+        });
+
+      });
+    </script>
+<script>
+    document.onkeydown=function(evt){
+        var keyCode = evt ? (evt.which ? evt.which : evt.keyCode) : event.keyCode;
+        if(keyCode == 13)
+        {
+            //your function call here
+            document.test.submit();
+        }
+    }
+</script>
 <div class="modal fade" id="mapModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-body">
-          <div style="width: 100%"><iframe class="map-custom" width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(Sofrah)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
-          </iframe>
+          <div style="width: 100%">
+<!--           
+          <iframe class="map-custom" width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(Sofrah)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+          </iframe> -->
+<?php echo $iframe;?>
+
+
           <div class="row">
               <div class="input-group input-group-lg location my-2 col-lg-12">
-                <input type="text" class="form-control" placeholder="Pakistan"  style="border-radius: 10px;border-color:#E1E1E1;" />
+              
+              
+              <!-- <input type="text" class="form-control" placeholder="Pakistan"  style="border-radius: 10px;border-color:#E1E1E1;" /> -->
+              
+              <form name="test" action="#" method="POST">
+<input type="text" name="search" />
+</form>
+              
                 <div class="input-group-btn">
                     <button class="fas fa-location ico"></button>
                 </div>
