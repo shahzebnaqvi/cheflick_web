4070100ad33136c4be66c189853313e4a4ee1790
unknown <shahzeb.naqvi12@gmail.com>
2022-06-28 19:28:53 +0500

    as

--- components/modals/map-modal.php
+++ components/modals/map-modal.php
@@ -1,4 +1,72 @@
 <?php
+
+// include_once 'constant.php';
+
+
+if(isset($_POST['login'])){
+
+  $baseurlapi="https://api.cheflick.net/api/";
+$url = $baseurlapi."user/user-location";
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$title=$_POST['title'];
+$address=$_POST['address'];
+$latitude=$_POST['latitude'];
+$longitude=$_POST['longitude'];
+$type=$_POST['type'];
+
+
+$headers = array(
+   "Accept: application/json",
+   "Content-Type: application/json",
+   "Authorization: Bearer ".$_SESSION['remember_token']."" 
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+$data= <<<DATA
+{
+  
+  "title":"$title",
+  "address":"$address",
+  "latitude":"$latitude",
+  "longitude":"$longitude",
+  "type":"$type"
+  
+  
+  }
+DATA;
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+$resp = curl_exec($curl);
+
+$obj = json_decode($resp,true);
+
+$obj = json_decode($resp,true);
+if($obj['status']){
+
+    print_r( $obj['data']);
+}
+else{
+
+}
+
+
+curl_close($curl);
+
+
+
+
+
+}
+?>
+
+
+
+<?php
 $iframe= ' <iframe class="map-custom" width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(Sofrah)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
   </iframe>';
   ?>
@@ -20,12 +88,17 @@ $iframe= ' <iframe class="map-custom" width="100%" height="400" frameborder="0" 
                     url : 'components/modals/map.php',
                     data : { sql : office},
                     success:function(result){
-                      $('.order_summary').html(result);
+                      $('.mapsearch').html(result);
                     	// alert(result);
                     }})
                 })
             })
 </script>
+
+
+
+
+
 <div class="modal fade" id="mapModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
@@ -36,9 +109,9 @@ $iframe= ' <iframe class="map-custom" width="100%" height="400" frameborder="0" 
           </iframe> -->
 
 
-<div class="order_summary" >
-  <?php echo $iframe;?>
- </div>
+        <div class="mapsearch" id="mapsearch">
+          <?php echo $iframe;?>
+         </div>
 
 
           <div class="row">
@@ -53,7 +126,7 @@ $iframe= ' <iframe class="map-custom" width="100%" height="400" frameborder="0" 
 
 
 
-
+              
 
               <input  class="form-control" placeholder="Pakistan"  style="border-radius: 10px;border-color:#E1E1E1;" type="text" name="location" id ="location" />
 
@@ -65,9 +138,10 @@ $iframe= ' <iframe class="map-custom" width="100%" height="400" frameborder="0" 
 
 
 
-                <div class="input-group-btn">
-                    <button class="fas fa-location ico"></button>
+                <div class="input-group-btn" >
+                    <button class="fas fa-location ico" onclick="getLocation()"></button>
                 </div>
+
             </div>
           </div>
           <div class="row">
@@ -87,3 +161,20 @@ $iframe= ' <iframe class="map-custom" width="100%" height="400" frameborder="0" 
   </div>
 </div>
 
+
+<script>
+var x = document.getElementById("mapsearch");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = '<iframe class="map-custom" width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q='  + position.coords.latitude +',' +position.coords.longitude+'&output=embed"></iframe>' ;
+
+}
+</script>
