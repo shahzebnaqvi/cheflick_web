3afeace2862ac06c44fb2077f3265d581d2967b7
unknown <shahzeb.naqvi12@gmail.com>
2022-07-13 22:18:34 +0500

    SA

--- ajax_add_to_cart.php
+++ ajax_add_to_cart.php
@@ -1,3 +1,6 @@
+<script type="text/javascript" src="jquery.leanModal.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+
 <?php
 session_start();
 // echo "sasas";
--- single-cafe.php
+++ single-cafe.php
@@ -441,6 +441,19 @@ include 'ajax_popup.php';
 
 
 
+<div class="modal fade" id="addedtocard" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">              
+      <div class="modal-body">
+      	<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <img src="" class="imagepreview" style="width: 100%;" >
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
 
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
