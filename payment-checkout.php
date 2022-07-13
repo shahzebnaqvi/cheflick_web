797c0dee0cb87239b121117c3dfc0039f45169c9
unknown <shahzeb.naqvi12@gmail.com>
2022-07-13 18:37:08 +0500

    SA

--- payment-checkout.php
+++ payment-checkout.php
@@ -23,113 +23,64 @@
             <div class="row my-4">
                 <div class="col-sm-12">
                 <div class="myCard cardMy">
-                    <div class="row">
-                        <div class="col-sm-1">
-                            <img class="heiImage" src="images/pizza.png" />
-                        </div>
-                        <div class="col-sm-3 ml-3">
-                            <p class="pizzaHead">Pizza Vlopper</p>
+                 
+                    
+
+                    
+    <?php		
+    $total_quantity =0;
+    $total_price=0;
+    foreach ($_SESSION["cart_item"] as $item){
+
+        $item_price = $item["quantity"]*$item["price"];
+		?>
+				
+
+
+                <div class="row">
+                       <div class="col-sm-1">
+                           <img class="heiImage" src="<?php echo $item["image"]; ?>" />
+                       </div>
+                       <div class="col-sm-3 ml-3">
+                           <p class="pizzaHead"><?php echo $item["name"]; ?></p>
                             <span class="purpleBack"><i class="fas fa-plus"></i></span>
-                            <span class="simpleBack">2</span>
-                            <span class="purpleBack"><i class="fas fa-minus"></i></span>
-                        </div>
-                        <div class="col-sm-2 d-flex align-items-center">
-                            <p class="serveText">Serves 2</p>
-                        </div>
-                        <div class="col-sm-5 d-flex justify-content-end align-items-center">
-                            <del class="strikeText">Rs.250</del><p class="serveText">Rs.150</p>
-                        </div>
-                    </div>
-                    <div class="dropdown-divider"></div>
-                    <div class="row">
-                        <div class="col-sm-1">
-                            <img class="heiImage" src="images/pizza.png" />
-                        </div>
-                        <div class="col-sm-3 ml-3">
-                            <p class="pizzaHead">Pizza Vlopper</p>
-                            <span class="purpleBack"><i class="fas fa-plus"></i></span>
-                            <span class="simpleBack">2</span>
-                            <span class="purpleBack"><i class="fas fa-minus"></i></span>
-                        </div>
-                        <div class="col-sm-2 d-flex align-items-center">
-                            <p class="serveText">Serves 2</p>
-                        </div>
-                        <div class="col-sm-5 d-flex justify-content-end align-items-center">
-                            <del class="strikeText">Rs.250</del><p class="serveText">Rs.150</p>
-                        </div>
-                    </div>
-                    <div class="dropdown-divider"></div>
-                    <div class="row">
-                        <div class="col-sm-1">
-                            <img class="heiImage" src="images/pizza.png" />
-                        </div>
-                        <div class="col-sm-3 ml-3">
-                            <p class="pizzaHead">Pizza Vlopper</p>
-                            <span class="purpleBack"><i class="fas fa-plus"></i></span>
-                            <span class="simpleBack">2</span>
-                            <span class="purpleBack"><i class="fas fa-minus"></i></span>
-                        </div>
-                        <div class="col-sm-2 d-flex align-items-center">
-                            <p class="serveText">Serves 2</p>
-                        </div>
-                        <div class="col-sm-5 d-flex justify-content-end align-items-center">
-                            <del class="strikeText">Rs.250</del><p class="serveText">Rs.150</p>
-                        </div>
-                    </div>
-                    <div class="dropdown-divider"></div>
-                    <div class="row">
-                        <div class="col-sm-1">
-                            <img class="heiImage" src="images/pizza.png" />
-                        </div>
-                        <div class="col-sm-3 ml-3">
-                            <p class="pizzaHead">Pizza Vlopper</p>
-                            <span class="purpleBack"><i class="fas fa-plus"></i></span>
-                            <span class="simpleBack">2</span>
-                            <span class="purpleBack"><i class="fas fa-minus"></i></span>
-                        </div>
-                        <div class="col-sm-2 d-flex align-items-center">
-                            <p class="serveText">Serves 2</p>
-                        </div>
-                        <div class="col-sm-5 d-flex justify-content-end align-items-center">
-                            <del class="strikeText">Rs.250</del><p class="serveText">Rs.150</p>
-                        </div>
-                    </div>
-                    <div class="dropdown-divider"></div>
-                    <div class="row">
-                        <div class="col-sm-1">
-                            <img class="heiImage" src="images/pizza.png" />
-                        </div>
-                        <div class="col-sm-3 ml-3">
-                            <p class="pizzaHead">Pizza Vlopper</p>
-                            <span class="purpleBack"><i class="fas fa-plus"></i></span>
-                            <span class="simpleBack">2</span>
-                            <span class="purpleBack"><i class="fas fa-minus"></i></span>
-                        </div>
-                        <div class="col-sm-2 d-flex align-items-center">
-                            <p class="serveText">Serves 2</p>
-                        </div>
-                        <div class="col-sm-5 d-flex justify-content-end align-items-center">
-                            <del class="strikeText">Rs.250</del><p class="serveText">Rs.150</p>
-                        </div>
-                    </div>
-                    <div class="dropdown-divider"></div>
-                    <div class="row">
-                        <div class="col-sm-1">
-                            <img class="heiImage" src="images/pizza.png" />
-                        </div>
-                        <div class="col-sm-3 ml-3">
-                            <p class="pizzaHead">Pizza Vlopper</p>
-                            <span class="purpleBack"><i class="fas fa-plus"></i></span>
-                            <span class="simpleBack">2</span>
-                            <span class="purpleBack"><i class="fas fa-minus"></i></span>
-                        </div>
-                        <div class="col-sm-2 d-flex align-items-center">
-                            <p class="serveText">Serves 2</p>
-                        </div>
-                        <div class="col-sm-5 d-flex justify-content-end align-items-center">
-                            <del class="strikeText">Rs.250</del><p class="serveText">Rs.150</p>
-                        </div>
-                    </div>
+                           <span class="simpleBack"><?php echo $item["quantity"]; ?></span>
+                           <span class="purpleBack"><i class="fas fa-minus"></i></span>
+                       </div>
+                       <div class="col-sm-2 d-flex align-items-center">
+                           <p class="serveText">Serves 2</p>
+                       </div>
+                       <div class="col-sm-5 d-flex justify-content-end align-items-center">
+                           <del class="strikeText">Rs.250</del><p class="serveText">Rs <?php echo $item["price"]; ?></p>
+                       </div>
+                       
+                   </div>
+                   <div class="dropdown-divider"></div>
+
+
+                      
+
+
+
+
+
+
+
+                
+                
+                
+                
+
+
+
+
+
+				<?php
+				$total_quantity += $item["quantity"];
+				$total_price += ($item["price"]*$item["quantity"]);
+                
+		}
+		?>
                 </div>
             </div>
             </div>
